3bddd46550136befd10d80330aa4ea1f73e844d2
kraken <mazurtolik87@gmail.com>
2019-08-05 18:41:31 +0300

    add login on server status

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ app.use('/', express.static(__dirname + '/docs'));
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
+console.log('Server success started on http://localhost:5000/');
