1c7aaa6dda3621e9752a112b29c8512953ff4922
kraken <mazurtolik87@gmail.com>
2019-08-12 18:40:26 +0300

    add function

--- docs/script.js
+++ docs/script.js
@@ -1,23 +1,86 @@
 "use strict";
 
 (function () {
-	var
-			list = [],
+	let
+			list = [
+				{
+					content: 'dsfa',
+					color: 'red'
+				},
+				{
+					content: 'dsfasgdf',
+					color: 'green'
+				},
+				{
+					content: 'dsdfdfg',
+					color: 'yellow'
+				}
+			],
 			elList = document.querySelector('#list'),
 			elButton = document.querySelector('#add-task'),
-			elInput = document.querySelector('#new-task');
+			elInput = document.querySelector('#new-task'),
+			elInputColor = document.querySelector('#color-task');
 
-	elButton.addEventListener('click', function () {
-		var value = elInput.value;
+	// Functions
+	function addItem() {
+		let value = elInput.value;
 
-		if(value.length < 1) {
+		if (value.length < 1) {
 			return;
 		}
 
-		list.push(value);
+		list.push({
+			content: value,
+			color: elInputColor.value
+		});
 		elInput.value = '';
+		renderList();
+		saveToLocalStorage()
+	}
 
-		console.log(list);
-	})
+	function addItemInDOM(item, index) {
+		let
+				elLi = document.createElement('li'),
+				elButton = document.createElement('button');
+
+		elButton.innerHTML = 'x';
+
+		elButton.addEventListener('click', function () {
+			removeItem(index)
+		});
+
+		elLi.innerHTML = item.content;
+		elLi.setAttribute('data-index', index);
+		elList.appendChild(elLi);
+
+		elLi.appendChild(elButton);
+
+		console.log(item.color);
+
+		if( item.color ) {
+			elLi.style.color = item.color;
+		}
+	}
+
+	function removeItem(index) {
+		list.splice(index, 1);
+		renderList();
+		saveToLocalStorage();
+	}
+
+	function renderList() {
+		elList.innerHTML = '';
+		for (let i = 0; i < list.length; i++) {
+			addItemInDOM(list[i], i)
+		}
+	}
+
+	function saveToLocalStorage() {
+		localStorage.setItem("kraken_items", JSON.stringify(list));
+	}
+
+	renderList();
+
+	elButton.addEventListener('click', addItem);
 
 })();
